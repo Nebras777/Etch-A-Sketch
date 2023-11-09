efb8b26eb876b9f55a32c759c3b5c3e56f4de279
Nebras777 <109689970+Nebras777@users.noreply.github.com>
2023-11-09 22:33:22 +0400

    Added the code for brushing
    Changing background color of the grid's divs upon click-dragging achieves this effect

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById('canvas')
+let mouseDown = false;
 
-for (let i = 1; i <= 64; i++) {
-    const content = document.createElement('div');
-    canvas.appendChild(content);
+canvas.onmousedown = () => (mouseDown = true)
+canvas.onmouseup = () => (mouseDown = false)
+
+function changeColor(e) {
+    if (!(e.type === 'mouseover' && !mouseDown)) {
+        e.target.style.backgroundColor = 'red'
+    }
+}
+
+for (let i = 1; i <= 16**2; i++) {
+    const content = document.createElement('div')
+    content.classList.add('box')
+    content.addEventListener('mouseover', changeColor)
+    content.addEventListener('mousedown', changeColor)
+    canvas.appendChild(content)
 }
