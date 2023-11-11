f6dc9c020f0beb53aea8447f085b6fca263d73b1
Nebras777 <109689970+Nebras777@users.noreply.github.com>
2023-11-11 19:31:42 +0400

    Add functionality to the three modes
    Color: color picker, Random: random rgb value, Rainbow: rainbow animation

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
+const INITIAL_COLOR = '#ff0000'
+const INITIAL_MODE = 'color'
+
 const canvas = document.getElementById('canvas')
+const colorPicker = document.getElementById('color-picker')
+const colorButton = document.getElementById('color-button')
+const randomButton = document.getElementById('random-button')
+const rainbowButton = document.getElementById('rainbow-button')
+
+let color = INITIAL_COLOR
+let mode = INITIAL_MODE
 let mouseDown = false;
 
 canvas.onmousedown = () => (mouseDown = true)
 canvas.onmouseup = () => (mouseDown = false)
+colorPicker.oninput = (e) => color = e.target.value
+colorButton.onclick = () => mode = 'color'
+randomButton.onclick = () => mode = 'random'
+rainbowButton.onclick = () => mode = 'rainbow'
 
 function changeColor(e) {
     if (!(e.type === 'mouseover' && !mouseDown)) {
-        e.target.style.backgroundColor = 'red'
+        if (mode === 'color') {
+            e.target.classList.remove('rainbow')
+            e.target.style.backgroundColor = color
+        } else if (mode === 'random') {
+            e.target.classList.remove('rainbow')
+            red = Math.floor(Math.random() * 256)
+            green = Math.floor(Math.random() * 256)
+            blue = Math.floor(Math.random() * 256)
+            e.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+        } else if (mode === 'rainbow') {
+            e.target.classList.add('rainbow')
+        }
     }
 }
 
@@ -17,3 +42,4 @@ for (let i = 1; i <= 16**2; i++) {
     content.addEventListener('mousedown', changeColor)
     canvas.appendChild(content)
 }
+
